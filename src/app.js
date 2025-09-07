2687a8d09ce67a400a034e73ea83631d8e7c64cc
William <dungnguyen260103@gmail.com>
2025-09-07 15:02:34 +0700

    init project

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+const app = express();
+
+//init middlewares
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
+//init db
+//init routes
+app.get('/', (req, res, next) => {
+    return res.status(200).json({message: 'Welcome to WSV eCommerce API'});
+});
+//handling errors
+module.exports = app;
