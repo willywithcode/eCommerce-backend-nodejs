f8a41005d207bb0aadafdeef68b44a7c1f69f979
William <dungnguyen260103@gmail.com>
2025-09-09 00:10:48 +0700

    setup config

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./src/app");
-const PORT =  3055;
+const PORT = process.env.PORT || 3055;
 
 const server = app.listen(PORT, () => {
   console.log(`WSV eCommerce start with port ${PORT}`);
--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -2,7 +2,8 @@
 
 const mongoose = require('mongoose');
 
-const connectString = 'mongodb://localhost:27017/shopDEV';
+const {db: {host, port, name}} = require('../configs/config.mongodb');
+const connectString = `mongodb://${host}:${port}/${name}`;
 
 class Database  {
     constructor() {
