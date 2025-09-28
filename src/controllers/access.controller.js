60c19c86b9d6b3eabd5779eca663b0c03422bde9
William <dungnguyen260103@gmail.com>
2025-09-28 15:10:03 +0700

    Refactor signUp response handling; implement SuccessResponse class for structured responses

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,10 +1,15 @@
 'use strict'
 
 const AccessService = require("../services/access.service");
+const {OK, CREATED} = require("../core/success.response");
 
 class AccessController {
     signUp = async (req, res, next) => {
-        return res.status(201).json(await AccessService.signUp(req.body));
+        new CREATED({
+            message: "Registered OK",
+            metadata: await AccessService.signUp(req.body),
+            options: { limit: 10, page: 1 }
+        }).send(res);
     }
 }
 
---
+++ src/core/success.response.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const StatusCode = {
+    OK: 200,
+    CREATED: 201,
+}
+const ReasonStatusCode = {
+    CREATED: 'Created',
+    OK: 'OK',
+}
+
+class SuccessResponse {
+    constructor({ message, statusCode = StatusCode.OK, reasonStatusCode = ReasonStatusCode.OK, metadata = {} }) {
+        this.message = !message ? reasonStatusCode : message;
+        this.status = statusCode;
+        this.reasonStatusCode = reasonStatusCode;
+    }
+    send(res, header = {}) {
+        return res.status(this.status).json(this);
+    }
+}
+class OK extends SuccessResponse {
+    constructor({ message, metadata }) {
+        super({ message, metadata });
+    }
+}
+
+class CREATED extends SuccessResponse {
+    constructor({message, statusCode = StatusCode.CREATED, reasonStatusCode = ReasonStatusCode.CREATED, metadata, options = {}}) {
+        super({ message, statusCode, reasonStatusCode, metadata });
+        this.options = options;
+    }
+}
+
+module.exports = {
+    OK, 
+    CREATED
+}
+
